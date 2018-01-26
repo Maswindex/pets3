69e991b3641600547cc3398bd65ff1e278605b2a
Mason Hernandez <mhernandez58@mail.greenriver.edu>
2018-01-26 17:22:15 -0500

    Created file for validatiion

---
+++ views/model/validate.php
@@ -0,0 +1,32 @@
+<?php
+    $errors = array();
+
+    /*
+   * Validate a color
+   *
+   * @param String color
+   * @return boolean
+   */
+    function validColor($color){
+        global $f3;
+        return in_array($color, $f3->get('colors'));
+    }
+
+    /*
+    * Validate a string
+    *
+    * @param String string
+    * @return boolean
+    */
+    function validString($string){
+        global $f3;
+        return ($string != null) && ( ctype_alpha($string));
+    }
+
+    if(!validColor($color)) {
+        $errors['color'] = "Please enter  valid color";
+    } else if(!validString($petname)) {
+        $errors['petname'] = "Please enter  valid name";
+    }
+
+    $success = sizeof($errors) == 0;
